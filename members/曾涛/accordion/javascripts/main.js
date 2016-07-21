48a2ae9cdecdd053a098253912eb40e32c51a67c
Jimmzy <429449830@qq.com>
2016-07-21 18:42:29 +0800

    commit

--- members/曾涛/accordion/javascripts/main.js
+++ members/曾涛/accordion/javascripts/main.js
@@ -1,13 +1,12 @@
-(function($) {
-    $.accordion = function(node) {
+(function ($) {
+    $.accordion = function (node) {
         var child = node.children().get(0).nodeName.toLowerCase();
-        node.on('click', child, function(e) {
+        node.on('click', child, function (e) {
             if ($(this).hasClass('selected')) {
                 $(this).removeClass('selected');
             } else {
                 $(this).addClass('selected')
-                    .siblings('.selected')
-                    .removeClass('selected');
+                .siblings('.selected').removeClass('selected');
             }
         });
     };
