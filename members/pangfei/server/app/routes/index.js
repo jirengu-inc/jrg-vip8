8a9f3fb06cd91a98d57bee49518c5e2d244acc4f
Faye1208 <358658874@qq.com>
2016-07-28 10:35:14 +0800

    modify

--- members/pangfei/server/app/routes/index.js
+++ members/pangfei/server/app/routes/index.js
@@ -1,12 +1,12 @@
 module.exports = function(app) {
-	var Router 		= require('koa-router'), 
+	var Router	= require('koa-router'), 
 		indexCtrl 	= require('../controllers/index');
 
 	var router = new Router();
 
 	router
 		.get('/', indexCtrl.index)
-        .post('/', indexCtrl.post)
+                .post('/', indexCtrl.post)
 
 	app.use(router.middleware());
 };
